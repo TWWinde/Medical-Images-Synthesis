17a8b767ff8b6d61825173c1a095b47b8ef72c7a
TWWinde <twwinde@gmail.com>
2023-09-27 15:20:58 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 python test.py --name medicals --dataset_mode medicals --gpu_ids 0 \
---dataroot /misc/data/private/autoPET --batch_size 20 --model_supervision 0  \
+--dataroot /misc/data/private/autoPET/data_nnunet --batch_size 20 --model_supervision 0  \
