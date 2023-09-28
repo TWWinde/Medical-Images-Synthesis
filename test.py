ce2e8fbe44caed61e2eebfec45d4629050098bd1
TWWinde <twwinde@gmail.com>
2023-09-28 23:58:03 +0200

    Update test.py

--- test.py
+++ test.py
@@ -55,13 +55,13 @@ def compute_iou(pred_mask, gt_mask):
 
 
 def compute_miou(pred_folder, gt_folder):
-    image_name = [f for f in sorted(os.listdir(pred_folder)) if f.endswith(".png")]
+    image_name_list = [f for f in sorted(os.listdir(pred_folder)) if f.endswith(".png")]
 
     num_classes = 37
     class_ious = np.zeros(num_classes)
     for class_idx in range(num_classes):
         ious = []
-        for pred_file, gt_file in image_name:
+        for image_name in image_name_list:
             #mask_img = np.array(Image.open(os.path.join(gt_folder, gt_file)).convert('L')).astype(np.uint8)
             #print(mask_img)
             pred_mask = np.array(Image.open(os.path.join(pred_folder, image_name))).astype(np.uint8) == class_idx
