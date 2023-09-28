0dfcc37d0961d3ba82fe31f1ba22d0e4ebd91875
TWWinde <twwinde@gmail.com>
2023-09-28 22:27:13 +0200

    Update test.py

--- test.py
+++ test.py
@@ -60,7 +60,9 @@ def compute_miou(pred_folder, gt_folder):
         ious = []
         for pred_file, gt_file in zip(pred_files, gt_files):
             pred_mask = np.array(cv2.imread(os.path.join(pred_folder, pred_file))) == class_idx
+            print('shape1',pred_mask.shape)
             gt_mask = np.array(cv2.imread(os.path.join(gt_folder, gt_file))) == class_idx
+            print('shape2',gt_mask.shape)
             iou = compute_iou(pred_mask, gt_mask)
             ious.append(iou)
 
