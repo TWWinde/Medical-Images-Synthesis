a8687ea3b454ddf5e3d60b0ce45745735c0c5868
TWWinde <twwinde@gmail.com>
2023-11-17 04:03:18 +0100

    update

--- utils/miou_folder/moose_segment.py
+++ utils/miou_folder/moose_segment.py
@@ -1,7 +1,7 @@
 from moosez import moose
 
 model_name = 'clin_ct_organs'
-input_dir = '/home/Documents/your_project/data/input'
-output_dir = '/home/Documents/your_project/data/output'
+input_dir = '/misc/no_backups/s1449/Medical-Images-Synthesis/results/medicals/MOOSEv2_data'
+output_dir = '/misc/no_backups/s1449/Medical-Images-Synthesis/results/medicals/moose_segment_results'
 accelerator = 'cuda'
 moose(model_name, input_dir, output_dir, accelerator)
