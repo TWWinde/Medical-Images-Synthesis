de8f92f597abaca323e176685652d3b5f7291547
TWWinde <twwinde@gmail.com>
2024-01-02 15:51:55 +0100

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -11,7 +11,7 @@
 #SBATCH --gpus=1
 #SBATCH --qos=batch
 # SBATCH --nodes=1
-# SBATCH --gpus=rtx_a5000:1
+#SBATCH --gpus=rtx_a5000:1
 # SBATCH --gpus=geforce_rtx_2080ti:1
 # SBATCH --gpus=geforce_gtx_titan_x:1
 
@@ -52,7 +52,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py --name Oasis_MRI --dataset_mode medicals 
 #--dataroot /misc/data/private/autoPET/CT_MR  \
 #--batch_size 4 --model_supervision 0 --add_mask \
 #--Du_patch_size 32 --netDu wavelet  \
-#--netG 9 --channels_G 64 \
+#--netG 9 --channels_G 16 \
 #--num_epochs 500
 
 #CUDA_VISIBLE_DEVICES=0 python test.py --name medicals --dataset_mode medicals --gpu_ids 0 \
