b5fe22293caf534d7067f7ec13934d4bdf973ba2
TWWinde <twwinde@gmail.com>
2024-01-09 21:51:17 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -23,20 +23,14 @@ module load cuda
 
 
 
-python test.py --name medicals --dataset_mode medicals --gpu_ids 0 \
---dataroot /misc/data/private/autoPET/data_nnunet --batch_size 20 --model_supervision 0  \
-
-
 #experiments_1
 CUDA_VISIBLE_DEVICES=0 python test.py --name Oasis_MRI --dataset_mode medicals --gpu_ids 0 \
 --dataroot /misc/data/private/autoPET/CT_MR  \
 --batch_size 20 --model_supervision 0 --add_mask \
---Du_patch_size 32 --netDu wavelet  \
 --netG 0 --channels_G 64
 
 #experiments_2
 #CUDA_VISIBLE_DEVICES=0 python train.py --name Wavelet_MRI --dataset_mode medicals --gpu_ids 0 \
 #--dataroot /misc/data/private/autoPET/CT_MR  \
 #--batch_size 4 --model_supervision 0 --add_mask \
-#--Du_patch_size 32 --netDu wavelet  \
 #--netG 9 --channels_G 16
