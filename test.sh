2f13980931f117ded185aa860877a8d125026cf3
TWWinde <twwinde@gmail.com>
2024-01-18 13:23:53 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -35,7 +35,7 @@ module load cuda
 #--batch_size 20 --model_supervision 0 --add_mask \
 #--netG 9 --channels_G 16
 
-CUDA_VISIBLE_DEVICES=0 python train.py --name medicals --dataset_mode medicals --gpu_ids 0 \
+CUDA_VISIBLE_DEVICES=0 python test.py --name medicals --dataset_mode medicals --gpu_ids 0 \
 --dataroot /misc/data/private/autoPET/data_nnunet  \
 --batch_size 20 --model_supervision 0 --add_mask \
 --netG 0 --channels_G 64 \
